7aef24ce338601493dc57622c127cb772c1b39cd
陶剑浩 <cugtjh@hotmail.com>
2018-07-22 13:11:32 +0800

    分通道处理函数

--- myLidar/ReadFile.cpp
+++ myLidar/ReadFile.cpp
@@ -1,5 +1,9 @@
 #include "ReadFile.h"
 
+#define BLUE true
+#define GREEN false
+
+
 //判断帧头是否正确
 bool isHeaderRight(uint8_t header[8])
 {
@@ -40,31 +44,32 @@ bool ReadFile::setFilename(char filename[100])
 	}
 }
 
-void ReadFile::readAll()
+void ReadFile::readBlueAll()
 {
 	unsigned int j = 0;
 	HS_Lidar hs;
 	
-	unsigned int length;
 	//把文件的位置指针移到文件尾获取文件长度
+	unsigned int length;
 	fseek(m_filePtr, 0L, SEEK_END);
 	length = ftell(m_filePtr);
-	cout << "Processing:";
-	
+	cout << "BLueChannelProcessing:";
 	
 	//首先定义流 output_stream  ios::out 示输出,ios::app表示输出到文件尾。
 	fstream output_stream;
-	output_stream.open("Output.txt", ios::out);
+	output_stream.open("BlueOut.txt", ios::out);
+
+	//设置输出流flag
+	WaveData::ostreamFlag = BLUE;
 
 	//遍历文件获取数据
 	do {
 		_fseeki64(m_filePtr, j * 8, SEEK_SET);
 
+		//寻找帧头
 		uint8_t header[8];
 		memset(header, 0, sizeof(uint8_t) * 8);
 		fread(header, sizeof(uint8_t), 8, m_filePtr);
-
-
 		if (isHeaderRight(header))
 		{
 			//处理数据的流程：
@@ -97,11 +102,77 @@ void ReadFile::readAll()
 		
 	} while (!feof(m_filePtr));
 
+	//文件结束退出
+	if (feof(m_filePtr) == 1)
+	{
+		output_stream.close();
+		cout << "finished！" << endl;
+	}
+}
+
+
+void ReadFile::readGreenAll()
+{
+	unsigned int j = 0;
+	HS_Lidar hs;
+
+	//把文件的位置指针移到文件尾获取文件长度
+	unsigned int length;
+	fseek(m_filePtr, 0L, SEEK_END);
+	length = ftell(m_filePtr);
+	cout << "GreenChannelProcessing:";
+
+	//首先定义流 output_stream  ios::out 示输出,ios::app表示输出到文件尾。
+	fstream output_stream;
+	output_stream.open("GreenOutput.txt", ios::out);
+
+	//设置输出流flag
+	WaveData::ostreamFlag = GREEN;
+
+	//遍历文件获取数据
+	do {
+		_fseeki64(m_filePtr, j * 8, SEEK_SET);
+
+		//寻找帧头
+		uint8_t header[8];
+		memset(header, 0, sizeof(uint8_t) * 8);
+		fread(header, sizeof(uint8_t), 8, m_filePtr);
+		if (isHeaderRight(header))
+		{
+			//处理数据的流程：
+			_fseeki64(m_filePtr, -8, SEEK_CUR);
+			hs.initData(m_filePtr);
+
+			WaveData mywave;
+			mywave.GetData(hs);
+			mywave.Filter(mywave.m_GreenWave, mywave.m_GreenNoise);
+			mywave.Resolve(mywave.m_GreenWave, mywave.m_GreenGauPra, mywave.m_GreenNoise);
+			mywave.Optimize(mywave.m_GreenWave, mywave.m_GreenGauPra);
+
+			//输出信息到文件
+			output_stream << mywave;
+
+			//文件指针偏移一帧完整数据的字节数：2688/8
+			j += 336;
+
+			//打印处理进程情况
+			cout.width(3);
+			cout << int(100 * 8 * j / length) << "%";
+			cout << "\b\b\b\b";
+
+		}
+		else
+		{
+			//可能会多出二段回波数据：uint16_t[CH.nL1] -> 2*n
+			j += 2;
+		}
+
+	} while (!feof(m_filePtr));
 
 	//文件结束退出
 	if (feof(m_filePtr) == 1)
 	{
 		output_stream.close();
-		cout << "读取结束！" << endl;
+		cout << "finished！" << endl;
 	}
 }
